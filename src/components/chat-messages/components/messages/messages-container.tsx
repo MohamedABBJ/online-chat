94b6a6caab54fad7297289111891adbbaaef7aa6
MohamedABBJ <mohamedabbj@gmail.com>
2024-08-22 06:57:37 -0400

    +

--- src/components/chat-messages/components/messages/messages-container.tsx
+++ src/components/chat-messages/components/messages/messages-container.tsx
@@ -4,8 +4,12 @@ import { useEffect } from "react";
 import getMesagesQuery from "@/db/get-messages-query";
 import AllMessages from "./components/all-messages";
 import NewMessage from "./components/new-messages";
+import { socket } from "@/app/socket";
 
 function MessagesContainer() {
+  socket.on("newMessage", (arg) => {
+    
+  });
   return (
     <Box className="h-full overflow-y-auto scroll-smooth">
       <AllMessages />
