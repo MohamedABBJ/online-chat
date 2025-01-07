161f9908483fd1ef8d4ab14bc6550deaf616e5f3
MohamedABBJ <mohamedabbj@gmail.com>
2025-01-07 19:56:53 -0400

    +: adding warnings and error messages

--- src/utils/dialogMessages.ts
+++ src/utils/dialogMessages.ts
@@ -1,13 +1,35 @@
 interface DialogMessagesProps {
   message: string;
   section: "chatList" | "chatContainer" | "topChatContainer";
+  category: "error" | "warning";
   id?: number;
 }
 
 const dialogMessages: DialogMessagesProps[] = [
   {
-    message: "message",
+    message: "Are you sure that you want to remove this user?",
     section: "chatList",
+    category: "warning",
+  },
+  {
+    message: "Are you sure that you want to block this user?",
+    section: "chatList",
+    category: "warning",
+  },
+  {
+    message: "This image weights more than 5Mb",
+    section: "chatContainer",
+    category: "error",
+  },
+  {
+    message: "An error happened sending the message, please try again",
+    section: "chatContainer",
+    category: "error",
+  },
+  {
+    message: "An error happened setting your profile picture, please try again",
+    section: "topChatContainer",
+    category: "error",
   },
 ];
 
