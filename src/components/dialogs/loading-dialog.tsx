125f63f622a8e64a3495ceaa4d20445859649e5e
MohamedABBJ <mohamedabbj@gmail.com>
2025-03-23 15:10:29 -0400

    +:adding sppiner animation for loading screen

--- src/components/dialogs/loading-dialog.tsx
+++ src/components/dialogs/loading-dialog.tsx
@@ -1,11 +1,20 @@
 "use client";
 
+import { OrbitProgress } from "react-loading-indicators";
 import { Dialog, DialogContent } from "../ui/dialog";
 
 function LoadingDialog() {
+  const spinnerColor = "#0099ff";
   return (
     <Dialog open={true}>
-      <DialogContent className="flex flex-col items-center"></DialogContent>
+      <DialogContent className="flex flex-col items-center">
+        <OrbitProgress
+          color={spinnerColor}
+          size="large"
+          text="loading"
+          textColor=""
+        />
+      </DialogContent>
     </Dialog>
   );
 }
