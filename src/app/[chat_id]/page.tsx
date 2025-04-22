0ecb1f05bc676ba00b894a92d414cb1dac119c88
MohamedABBJ <mohamedabbj@gmail.com>
2025-04-22 05:28:00 -0400

    fix: unauthorized users can access private chats

--- src/app/[chat_id]/page.tsx
+++ src/app/[chat_id]/page.tsx
@@ -1,5 +1,6 @@
 import privateChatIDQuery from "@/db/private-chat-id-query";
 import UserSessionProps from "@/interfaces/user-session-props";
+import { redirect } from "next/navigation";
 import verifyUserSession from "../lib/dal";
 import Home from "../page";
 
@@ -7,12 +8,20 @@ async function Page({ params }: { params: { chat_id: string } }) {
   const session: UserSessionProps =
     (await verifyUserSession()) as UserSessionProps;
 
-  const checkPrivateChat = await privateChatIDQuery({
-    chat_id: params.chat_id,
-    user_id: session.user?.id as string,
-  });
-  //need to place two type of errors here a 404 and a 401
-  return <>{checkPrivateChat ? <Home chat_id={params.chat_id} /> : null}</>;
+  const checkPrivateChat =
+    session &&
+    (await privateChatIDQuery({
+      chat_id: params.chat_id,
+      user_id: session.user?.id as string,
+    }));
+
+  !checkPrivateChat && redirect("/");
+
+  return (
+    <>
+      <Home chat_id={params.chat_id} />
+    </>
+  );
 }
 
 export default Page;
